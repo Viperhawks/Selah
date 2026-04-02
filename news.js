1b53a2a38e7b3872ab0b10623c901adf057a4901
Astel Antony <tintupro1@gmail.com>
2026-04-02 21:33:01 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,18 +1,13 @@
 export default async function handler(req, res) {
   try {
     const response = await fetch(
-      "https://gnews.io/api/v4/search?q=Christianity&lang=en&max=5&apikey=2db2b5d6f2b15d84ca9f982b19c56d43"
-    );
+      "https://gnews.io/api/v4/search?q=Christianity&lang=en&max=5&apikey=2db2b5d6f2b15d84ca9f982b19c56d43
+      );
 
     const data = await response.json();
 
-    if (!data.articles) {
-      return res.status(200).json([]);
-    }
-
-    res.status(200).json(data.articles);
-
-  } catch (error) {
-    res.status(500).json({ error: "API failed" });
+    res.status(200).json(data.articles || []);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch news" });
   }
 }
